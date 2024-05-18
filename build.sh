6a6aeff6827a0c354546b846e2e9bfb9ac67dd61
Mirrovex <mirrovex@wp.pl>
2024-05-18 21:44:44 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3 manage.py collectstatic --clear
+python3 manage.py collectstatic --noinput --clear 

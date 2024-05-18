6d9a9d861fa413c7b6b814f4e2fcd6cc932adf35
Mirrovex <mirrovex@wp.pl>
2024-05-18 22:26:27 +0200

    libjpeg

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear 
+python3 manage.py collectstatic --noinput --clear
+
+apt-get install libjpeg62

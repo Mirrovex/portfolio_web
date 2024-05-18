96a6c8671b42cda442c5ad4fbba52c9113e8423d
Mirrovex <mirrovex@wp.pl>
2024-05-18 22:51:45 +0200

    libjpeg

--- build.sh
+++ build.sh
@@ -12,22 +12,5 @@ echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
 
 
-if command -v apt-get &> /dev/null; then
-    echo "System operacyjny: Debian/Ubuntu"
-    # Tutaj użyj komendy apt-get
-    # Na przykład:
-    # apt-get update
-    # apt-get install -y nazwa_pakietu
-# Sprawdź, czy apk jest dostępny (Alpine Linux)
-elif command -v apk &> /dev/null; then
-    echo "System operacyjny: Alpine Linux"
-    # Tutaj użyj komendy apk
-    # Na przykład:
-    # apk update
-    # apk add nazwa_pakietu
-else
-    echo "Nieznany system operacyjny"
-fi
-
-apk search libjpeg
-apk add libjpeg62
+docker run -it nazwa_obrazu /bin/sh
+apk --no-cache add libjpeg-turbo-dev

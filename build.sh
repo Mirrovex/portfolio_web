181675f8a42168daf8f424c82c37f08386cc89ad
Filip Mickiewicz <69217021+Mirrovex@users.noreply.github.com>
2024-05-19 20:06:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,26 +2,24 @@
 
 # Build the project
 
-#echo "Installing libjpeg..."
-#yum install libjpeg libjpeg-dev libjpeg-devel libpng-devel libjpeg-turbo libjpeg-turbo-devel --assumeyes
 
-
-echo "Sprawdzanie instalacji libjpeg:"
-yum update -y && yum install -y libjpeg-dev libjpeg
-
-ls -l /usr/lib64/libjpeg*
-ls -l /usr/lib/libjpeg*
-
-# Tworzenie symbolic linku do libjpeg.so.62
-ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so.62 /usr/lib/libjpeg.so.62
+echo " -> LIBJPEG"
 
 #echo "Libjpeg settings..."
-find /usr/lib* -name "libjpeg.so.62"
+#find /usr/lib* -name "libjpeg.so.62"
 #echo $LD_LIBRARY_PATH
+
+
+# Aktualizacja pakietów i instalacja libjpeg-turbo
+yun update -y && yun install -y libjpeg-turbo
 
 
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
+
+# Sprawdzenie dostępności libjpeg w Pillow
+python -c "from PIL import Image; print('JPEG support:', 'jpeg' in Image.registered_extensions().values())"
+
 
 echo "Make Migrations..."
 python3 manage.py makemigrations --noinput

dca12f32df7bc990fbba04d208534aa4c11bb58d
Mirrovex <mirrovex@wp.pl>
2024-05-21 18:39:36 +0200

    loading

--- main/models.py
+++ main/models.py
@@ -15,12 +15,25 @@ class Page(models.Model):
     
     def get_url(self):
         return self.reverse_url + self.slug
+    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    value = models.CharField(max_length=100, unique=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Settings(models.Model):
     name = models.CharField(max_length=100, unique=True)
     pages = models.ManyToManyField(Page)
     page_title = models.CharField(max_length=50, blank=True)
+    contacts = models.ManyToManyField(Contact)
 
     def __str__(self) -> str:
         return self.name
